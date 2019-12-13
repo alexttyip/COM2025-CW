fb7cebcbb1d0287c6bc455917e2c2a49da181516
Yip Tsz To <tsztoyip@gmail.com>
2019-12-13 23:33:08 +0000

    Hid workouts index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :users do
-    resources :workouts
+    resources :workouts, except: [:index]
   end
 
   root "users#index"
+  get "/users/:id/workouts", to: "users#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
