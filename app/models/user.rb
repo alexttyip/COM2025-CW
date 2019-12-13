ddb09352676ce3f82c58ae822d947ea2a2f42ee5
Yip Tsz To <tsztoyip@gmail.com>
2019-12-13 20:50:00 +0000

    Added validation and edited form

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   enum sex: [:male, :female, :others]
 
   has_many :workouts, dependent: :destroy
+
+  validates_presence_of :first_name, :last_name, :dob, :sex
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,3 +1,6 @@
 class Workout < ApplicationRecord
   belongs_to :user
+
+  validates_presence_of :title, :intensity, :date, :desc
+  validates_numericality_of :intensity, only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10
 end
