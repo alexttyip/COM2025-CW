7ebb2327eedc29f4912b28c8592a7156b7e451e4
Yip Tsz To <tsztoyip@gmail.com>
2019-12-14 13:30:29 +0000

    add home and devise

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
     resources :workouts, except: [:index]
   end
 
-  root "users#index"
+  #root "users#index"
+  root 'home#home'
   get "/users/:id/workouts", to: "users#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
