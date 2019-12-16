8007508fec44438a4d8ee13a0c315a94aee008a9
Yip Tsz To <tsztoyip@gmail.com>
2019-12-16 12:17:14 +0000

    Added contact mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'info@workouttracker.com', to: 'info@workouttracker.com'
   layout 'mailer'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, path: "u"
 
   root 'home#home'
+  get 'contact', to: "home#contact"
+  post 'request_contact', to: "home#request_contact"
+
   get "/u/:username", to: "users#index", as: "users"
 
   resources :users, path: "u" do
